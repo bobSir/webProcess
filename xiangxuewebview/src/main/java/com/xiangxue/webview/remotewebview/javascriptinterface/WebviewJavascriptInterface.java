ae60ce794bdd7dc5b20e570bc8be647f66995434
chenliangyu <3063093332@qq.com>
2020-07-10 15:31:01 +0800

    add

---
+++ xiangxuewebview/src/main/java/com/xiangxue/webview/remotewebview/javascriptinterface/WebviewJavascriptInterface.java
@@ -0,0 +1,48 @@
+package com.xiangxue.webview.remotewebview.javascriptinterface;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+
+import java.util.logging.Logger;
+
+/**
+ * 1. 保留command的注册
+ * 2. 不支持command通过远程aidl方式调用
+ */
+public final class WebviewJavascriptInterface {
+
+    private final Context mContext;
+    private final Handler mHandler = new Handler();
+    private JavascriptCommand javascriptCommand;
+
+    public WebviewJavascriptInterface(Context context) {
+        mContext = context;
+    }
+
+    @JavascriptInterface
+    public void post(final String cmd, final String param) {
+        Log.d("@cly", "post -- " + cmd + " -- " + param);
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if (javascriptCommand != null) {
+                        javascriptCommand.exec(mContext, cmd, param);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void setJavascriptCommand(JavascriptCommand javascriptCommand) {
+        this.javascriptCommand = javascriptCommand;
+    }
+
+    public interface JavascriptCommand {
+        void exec(Context context, String cmd, String params);
+    }
+}
